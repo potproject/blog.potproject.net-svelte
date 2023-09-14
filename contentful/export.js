b6c19f5ec4d6b80acb16d158e8803afcb2dd7c35
potproject <pptppctt@gmail.com>
2023-09-14 14:40:29 +0900

    rss fix

--- contentful/export.js
+++ contentful/export.js
@@ -104,24 +104,6 @@ const generatePageList = (entries) => {
   console.log(`Page list file has been created: ${pageListFile}`)
 }
 
-const generateLatest = (entries) => {
-  const latest = []
-  for (const entry of entries.slice(0, 10)) {
-    if (!entry.fields.hiddenPage || !entry.fields.hiddenPage[CONTENTFUL_DEFAULT_LOCALE]) {
-      latest.push({
-        id: entry.sys.id,
-        title: entry.fields.title[CONTENTFUL_DEFAULT_LOCALE],
-        url: generateUrl(entry),
-      })
-    }
-  }
-
-  // latest file write
-  const latestFile = path.join(staticDirectory, 'latest.json')
-  fs.writeFileSync(latestFile, JSON.stringify(latest, null, 2))
-  console.log(`Latest file has been created: ${latestFile}`)
-}
-
 const generateRss = (entries) => {
 	const feed = new Feed({
 		title: 'blog.potproject.net',
@@ -132,22 +114,33 @@ const generateRss = (entries) => {
 		image: `${baseUrl}/author.png`,
 		copyright: 'potproject',
 		generator: 'Sveltekit',
+    feedLinks: {
+      atom: "${baseUrl}/atom.xml",
+    },
 		author: {
 		  name: 'potpro',
 		  //email: "johndoe@example.com",
 		  link: 'https://potproject.net',
 		},
+
 	  })
 
 	for (const entry of entries.slice(0, 20)) {
 		if (!entry.fields.hiddenPage || !entry.fields.hiddenPage[CONTENTFUL_DEFAULT_LOCALE]) {
+      let md = entry.fields.content[CONTENTFUL_DEFAULT_LOCALE]
+      md = md.replace(/^(.+)\n/, '')
+      const plainText = removeMd(md).replace(/\n/g, ' ').trim()
+
+      let description = plainText.slice(0, 300)
+      if (plainText.length > 300) {
+        description += '...'
+      }
 			const url = generateUrl(entry);
-			const html = removeMd(entry.fields.content[CONTENTFUL_DEFAULT_LOCALE]);
 			feed.addItem({
 				title: entry.fields.title[CONTENTFUL_DEFAULT_LOCALE],
 				id: `${baseUrl}${url}`,
 				link: `${baseUrl}${url}`,
-				description: html,
+				description: description,
 				date: new Date(entry.fields.createdDate[CONTENTFUL_DEFAULT_LOCALE]),
 				image: entry.fields.headerImgur ? entry.fields.headerImgur[CONTENTFUL_DEFAULT_LOCALE] : null,
 			})
@@ -158,6 +151,11 @@ const generateRss = (entries) => {
 	const rssFile = path.join(staticDirectory, 'rss.xml')
 	fs.writeFileSync(rssFile, feed.rss2())
 	console.log(`RSS file has been created: ${rssFile}`)
+
+  // atom file write
+  const atomFile = path.join(staticDirectory, 'atom.xml')
+  fs.writeFileSync(atomFile, feed.atom1())
+  console.log(`Atom file has been created: ${atomFile}`)
 }
 
 const execute = (entries) => {
@@ -171,6 +169,5 @@ const execute = (entries) => {
   generateRedirectPath(entries)
   generatePageList(entries)
   generateRss(entries)
-  generateLatest(entries)
   console.log('Done! Total files: ' + entries.length)
 }
